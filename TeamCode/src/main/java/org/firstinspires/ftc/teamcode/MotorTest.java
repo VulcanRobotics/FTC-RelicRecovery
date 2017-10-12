c957183da95a35017ec35779f51314a3b6ad29a0
agoldberg <agoldberg@sch.org>
2017-10-12 17:59:20 -0400

    Hey this is my first mecanum code yo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by agoldberg on 9/26/17.
+ */
+
+public abstract class MotorTest extends OpMode {
+}
