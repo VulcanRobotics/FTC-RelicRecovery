d93107b664310ac4ffec34d578c0cc92c37705ab
Ron Rossbach <rossbach.ron@gmail.com>
2017-10-30 17:19:30 -0400

    started adding fields to JewelAuton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,10 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.sch.ftc4914.Driveable;
+import org.sch.ftc4914.VladimirOmni;
 
 /**
  * Created by agoldberg on 9/26/17.
  */
+@TeleOp(name = "MotorTest")
+public class MotorTest extends OpMode {
+    VladimirOmni omniDrive;
+    double[] wheelSpeeds;
 
-public abstract class MotorTest extends OpMode {
+    @Override
+    public void init() {
+        omniDrive = new VladimirOmni(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.x) {
+            wheelSpeeds = omniDrive.maxWheelSpeeds();
+            telemetry.addData("Left: ", wheelSpeeds[0]);
+            telemetry.addData("Right: ", wheelSpeeds[1]);
+        } else {
+            omniDrive.omniDrive(0,0);
+        }
+    }
 }
