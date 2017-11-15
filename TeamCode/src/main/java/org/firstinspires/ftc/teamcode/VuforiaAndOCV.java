676e5b547f1e958012dc420651f7232605026d92
Ron Rossbach <rossbach.ron@gmail.com>
2017-11-15 17:09:54 -0500

    testing VladimirEye with front-facing camera - seems to work fine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaAndOCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaAndOCV.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.sch.ftc4914.VladimirEye;
 
 /**
  * Created by Ron on 11/11/2017.
@@ -15,24 +16,21 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 @TeleOp(name = "VuforiaAndOCV")
 
 public class VuforiaAndOCV extends OpMode {
-    private VuforiaLocalizer vuforia;
-    private VuforiaTrackables pictographTrackables;
-    private VuforiaTrackable pictographTemplate;
-    private RelicRecoveryVuMark pictograph;
+    private VladimirEye eye;
 
     @Override
     public void init() {
         telemetry.addData("Status:","init() Starting");
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = "AZl2c9X/////AAAAGbmMYWHiOErNgvxJkJoCOPVIXkX6NaLEPKi2f8UqbBQsBcb2ZpS6I3XlTgSPcZubKpQUcjByBtH6AbLGb7iqNcCwn8q4wqOJnZMmKMvnmZaCaGzF95/SOg05pk1bOoavzjOtD3AR8eeQsxWIQSVU7SwqxSWu4tXpClNY0+82DMI7Itz8RiZP/y7tJZ66LMhe5YOiKeBaTuPP/UoKjZQl9gtUWwhU0n79n47An+LywGjWkIVqaZsv28XjFt66dql+ZtmpC/uqadfWc+FYwxyypGmAvcjkpeAmQEABmjtOMCHjk0xgDZjKZ4hNK2k18rMKROwo/R7lWRU/D2DRGUyF1W1deELVLVemkAJG8AnJlHn/";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        eye = new VladimirEye(hardwareMap, VuforiaLocalizer.CameraDirection.FRONT);
+    }
 
+    @Override
+    public void start() {
+        eye.startLooking();
     }
 
     @Override
     public void loop() {
-
+        telemetry.addData("VuMark: ", eye.getPictograph());
     }
 }
---
+++ TeamCode/src/main/java/org/sch/ftc4914/VladimirEye.java
@@ -0,0 +1,45 @@
+package org.sch.ftc4914;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by Ron on 11/15/2017.
+ */
+
+public class VladimirEye {
+    private VuforiaLocalizer vuforia;
+    private HardwareMap hwMap;
+    private VuforiaTrackables relicTrackables;
+    private VuforiaTrackable relicPictograph;
+
+    public VladimirEye(HardwareMap hw, VuforiaLocalizer.CameraDirection cameraDirection) {
+        hwMap = hw;
+        int cameraMonitorViewId = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AZl2c9X/////AAAAGbmMYWHiOErNgvxJkJoCOPVIXkX6NaLEPKi2f8UqbBQsBcb2ZpS6I3XlTgSPcZubKpQUcjByBtH6AbLGb7iqNcCwn8q4wqOJnZMmKMvnmZaCaGzF95/SOg05pk1bOoavzjOtD3AR8eeQsxWIQSVU7SwqxSWu4tXpClNY0+82DMI7Itz8RiZP/y7tJZ66LMhe5YOiKeBaTuPP/UoKjZQl9gtUWwhU0n79n47An+LywGjWkIVqaZsv28XjFt66dql+ZtmpC/uqadfWc+FYwxyypGmAvcjkpeAmQEABmjtOMCHjk0xgDZjKZ4hNK2k18rMKROwo/R7lWRU/D2DRGUyF1W1deELVLVemkAJG8AnJlHn/";
+        parameters.cameraDirection = cameraDirection;
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicPictograph = relicTrackables.get(0);
+        relicPictograph.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+    }
+
+    public void startLooking() {
+        relicTrackables.activate();
+    }
+
+    public void stopLooking() {
+        relicTrackables.deactivate();
+    }
+
+    public RelicRecoveryVuMark getPictograph() {
+        return RelicRecoveryVuMark.from(relicPictograph);
+    }
+
+}
