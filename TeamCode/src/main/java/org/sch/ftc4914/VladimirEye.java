a4f5f0fefae98efff38156e3b5c81b97a749509c
Ron Rossbach <rossbach.ron@gmail.com>
2017-11-15 16:42:27 -0500

    updated VladimirEye to take camera direction parameter (front camera or back camera)

--- TeamCode/src/main/java/org/sch/ftc4914/VladimirEye.java
+++ TeamCode/src/main/java/org/sch/ftc4914/VladimirEye.java
@@ -26,12 +26,12 @@ public class VladimirEye {
     private VuforiaTrackables relicTrackables;
     private VuforiaTrackable relicPictograph;
 
-    public VladimirEye(HardwareMap hw) {
+    public VladimirEye(HardwareMap hw, VuforiaLocalizer.CameraDirection cameraDirection) {
         hwMap = hw;
         int cameraMonitorViewId = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = "AZl2c9X/////AAAAGbmMYWHiOErNgvxJkJoCOPVIXkX6NaLEPKi2f8UqbBQsBcb2ZpS6I3XlTgSPcZubKpQUcjByBtH6AbLGb7iqNcCwn8q4wqOJnZMmKMvnmZaCaGzF95/SOg05pk1bOoavzjOtD3AR8eeQsxWIQSVU7SwqxSWu4tXpClNY0+82DMI7Itz8RiZP/y7tJZ66LMhe5YOiKeBaTuPP/UoKjZQl9gtUWwhU0n79n47An+LywGjWkIVqaZsv28XjFt66dql+ZtmpC/uqadfWc+FYwxyypGmAvcjkpeAmQEABmjtOMCHjk0xgDZjKZ4hNK2k18rMKROwo/R7lWRU/D2DRGUyF1W1deELVLVemkAJG8AnJlHn/";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.cameraDirection = cameraDirection;
         vuforia = ClassFactory.createVuforiaLocalizer(parameters);
         relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
         relicPictograph = relicTrackables.get(0);
